8229cad0d76093b7f911440935dd1804cee8c91d
dream9ma <61941832+dream9ma@users.noreply.github.com>
2020-05-11 23:27:23 +0900

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import routes from "../routes";
-import { users, editProfile, userDetail, changePassword } from "../controllers/userController";
+import {
+  users,
+  editProfile,
+  userDetail,
+  changePassword,
+} from "../controllers/userController";
 
-const userRouter = express.Router(); 
+const userRouter = express.Router();
 
-userRouter.get(routes.users,users;
-userRouter.get(routes.editProfile,editProfile);
-userRouter.get(routes.userDetail,userDetail);
-userRouter.get(routes.changePassword,changePassword);
+userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
