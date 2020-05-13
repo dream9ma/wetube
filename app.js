8f1fd3f79b4e2e867fdfd5e96665c80d941a1b6e
dream9ma <61941832+dream9ma@users.noreply.github.com>
2020-05-13 22:02:23 +0900

    f

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan("dev"));
 app.set("view engine", "pug");
-app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,12 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "search", searchingBy });
+};
+
 export const videos = (req, res) =>
   res.render("Videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
@@ -9,5 +17,3 @@ export const editvideos = (req, res) =>
   res.render("Edit Video", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
   res.render("Delete Video", { pageTitle: "Delete Video" });
-export const search = (req, res) =>
-  res.render("Search", { pageTitle: "Search Video" });
