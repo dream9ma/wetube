958bd71e95c5c0a4b99276f37f95610431945649
dream9ma <61941832+dream9ma@users.noreply.github.com>
2020-05-17 22:08:45 +0900

    2. end

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,36 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  console.log(req.body);
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  console.log("this");
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    //To Do: Register User
+    //To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("Login", { pageTitle: "Log In" });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pateTitle: "User Detail" });
 export const editProfile = (req, res) =>
